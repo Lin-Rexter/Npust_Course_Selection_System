0798710eb11258fdf677d19c960e6f9d30d6efe0
Lin-Rexter <aaaa35963596@gmail.com>
2022-12-25 14:58:33 +0800

    20221215 0258

--- README.md
+++
@@ -1,2 +0,0 @@
-# Npust_Course_Selection_System
-屏科大模擬選課系統 Npust Course Selection System
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    $logout = @$_POST['logout'];
-    if ($logout != "") {
-        session_destroy();
-        header("refresh:0;url='index.php'");
-    }
-?>
